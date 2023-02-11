1d41a2afad432e814b4a520948954c06c6f3c856
Juleus Louis (Jay) Alquizalas <juleuslouis@gmail.com>
2023-02-10 19:40:04 -0700

    Prop-it-up enhanced for Putting it together

---
+++ React/prop-it-up/client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard personName="jay" lastName = "Doe" personAge= {50} personHaircolor="black" />
+      <PersonCard personName="juleus" lastName = "Re" personAge={51} personHaircolor="brown" />
+      <PersonCard personName="louis" lastName = "Mi" personAge={49} personHaircolor="auburn" />
+      
+      
+    </div>
+  );
+}
+
+export default App;
