53bddc328301c86cf717ff875c16249b7b6b8c83
Juleus Louis (Jay) Alquizalas <juleuslouis@gmail.com>
2023-02-19 15:39:07 -0700

    updting faker

--- React/faker_api/server/server.js
+++ React/faker_api/server/server.js
@@ -1,70 +1,45 @@
-// these top should always begin
+// dependencies
 const express = require("express")
 const app = express()
+
+//config port
 const port = 8000
 
+// express config
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // --- end ---
 
 
-const users = [
-    { name: "Jane Fonda", company: "Leotards are us" },
-    { name: "Marylyn Monroe", company: "MGM Studios"  },
-    { name: "Tom Cruise", company: "Top gun studios"  },
+// const users = [
+//     { name: "Jane Fonda", company: "Leotards are us" },
+//     { name: "Marylyn Monroe", company: "MGM Studios"  },
+//     { name: "Tom Cruise", company: "Top gun studios"  },
     
-]
+// ]
 
 
-//get all infomration
-//takes 2 arguments, path and call back function (a call back -- ()=>{}  )
-app.get("/api/users", (req, res) => {
-    res.json({
-        count: users.length,
-        results: users
-    })
+
+//create 
+class Someone{
+    constructor(){
+        // this.name = faker.name.firstName()
+   }
+}
+
+
+const { faker } = require('@faker-js/faker')
+
+app.get("/api/randomname", ({req,res}) => {
+    const randomname = faker.name.findName()
+    res.json(randomname)
 })
 
-
-// just get 1 result
-//      "passInPath"path variable   
-app.get("/api/users/:idx", (req, res) => {
-    res.json({ results: users[req.params.idx] })
+app.get("/api/someone/new", (req,res) => {
+    const newPerson = new Someone ()
+    res.json(randomname)
 })
-
-
-//create push/ add a users item
-app.post("/api/users", (req, res) => {
-    console.log(req.body)
-    users.push(req.body)
-    res.json({
-        count: users.length,
-        results: users
-    })
-})
-
-
-//update 1 item
-app.put("/api/users/:idx", (req, res) => {
-    users[req.params.idx] = req.body;
-    res.json({
-        count: users.length,
-        results: users
-    })
-})
-
-
-//delete item based on index (idx)
-app.delete("/api/users/:idx", (req, res) => {
-    users.splice(req.params.idx, 1)
-    res.json({
-        count: users.length,
-        results: users
-    })
-})
-
-
 
 
 // these should be at the bottom
